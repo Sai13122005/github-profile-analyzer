9fde06b686ac2e38f134e35a4a062a602c9244dd
Sai13122005 <pentasaikrishna00@gmail.com>
2026-06-10 10:59:27 +0530

    Added GitHub token authentication

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,12 +1,18 @@
 const axios = require("axios");
 
 const fetchGithubProfile = async (username) => {
+  const headers = {
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+  };
+
   const userResponse = await axios.get(
-    `https://api.github.com/users/${username}`
+    `https://api.github.com/users/${username}`,
+    { headers }
   );
 
   const repoResponse = await axios.get(
-    `https://api.github.com/users/${username}/repos`
+    `https://api.github.com/users/${username}/repos`,
+    { headers }
   );
 
   return {
